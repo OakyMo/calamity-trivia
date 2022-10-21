1d57d77fed7bd0a2fa42a5bff2c63bad2be9f60d
OakyMo <114772303+OakyMo@users.noreply.github.com>
2022-10-21 07:21:43 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import random
 import time
 from datetime import datetime, timedelta
 from os import listdir, path
